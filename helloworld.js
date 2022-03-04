73552cc94ae6a9c9b65f6e3b92100a035eb61921
piemanabc <67368507+piemanabc@users.noreply.github.com>
2022-03-04 22:45:31 +1300

    made files and got bar running

---
+++ helloworld.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var fs = require('fs');
+//var Plotly = require('./plotly-2.9.0.min.js')
+
+http.createServer(function (req, res) {
+  fs.readFile('chart.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
