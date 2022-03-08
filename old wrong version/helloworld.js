bfcb694ae33ab14c9a89a62236a9470bf5f6c878
piemanabc <67368507+piemanabc@users.noreply.github.com>
2022-03-08 19:46:48 +1300

    Added new code from tutorial

--- old wrong version/helloworld.js
+++ old wrong version/helloworld.js
@@ -3,9 +3,12 @@ var fs = require('fs');
 //var Plotly = require('./plotly-2.9.0.min.js')
 
 http.createServer(function (req, res) {
-  fs.readFile('index.php', function(err, data) {
+  fs.readFile('chart (2).html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
   });
 }).listen(8080);
+
+
+setInterval(console.log(parseInt(document.getElementById("slider").value)), 100);
